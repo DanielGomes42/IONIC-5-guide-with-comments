5e6e2e7b528097b60de3ad2caeadccecd07d18ac
DanielGomes42 <damsilva757@gmail.com>
2021-12-17 22:11:55 -0300

    corrected reorder

--- src/app/reorder/reorder-routing.module.ts
+++ src/app/reorder/reorder-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ReorderGroupExamplePage } from './reorder-group-example.page';
+import { ReorderPage } from './reorder.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: ReorderGroupExamplePage
+    component: ReorderPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class ReorderGroupExamplePageRoutingModule {}
+export class ReorderPageRoutingModule {}
