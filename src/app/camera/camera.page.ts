21cf41a21c97b046954b00fa6bb554a2c9f58876
DanilGomes42 <daniel757@gmail.com>
2021-12-29 18:48:44 -0300

    add  list

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -24,12 +24,10 @@ tirarfoto()
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE
     };
-    this.camera.getPicture(options).then((imageData) => {
-      // imageData is either a base64 encoded string or a file URI
-      // If it's base64 (DATA_URL):
+    this.camera.getPicture(options).then((imageData) => 
+    {
+
       let base64Image = 'data:image/jpeg;base64,' + imageData;
-     }, (err) => {
-      // Handle error
-     });
+     }, (err) => {});
 }
 }
