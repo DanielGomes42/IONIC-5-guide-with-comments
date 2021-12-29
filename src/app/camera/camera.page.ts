f72049b4da8e9e0d33c5e665c2bc698472bd7072
DanilGomes42 <daniel757@gmail.com>
2021-12-29 13:01:27 -0300

    camera

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+                                          
 
 @Component({
-  selector: 'app-camera',
+  selector: 'app-camera',                      
   templateUrl: './camera.page.html',
   styleUrls: ['./camera.page.scss'],
 })
 export class CameraPage implements OnInit {
 
-  constructor() { }
+foto:any;
+
+constructor(private camera: Camera) { }
 
   ngOnInit() {
   }
-
+tirarfoto() 
+{
+    const options: CameraOptions = 
+    {
+    quality: 100,
+    destinationType: this.camera.DestinationType.FILE_URI,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+    };
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
+}
 }
