112967187c8e036fe2ddfdfef61232aca8f2a7b1
DanielGomes42 <damsilva757@gmail.com>
2022-01-31 16:47:25 -0300

    up

--- src/app/componentes-visuais/chekbox/chekbox.page.ts
+++ src/app/componentes-visuais/chekbox/chekbox.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chekbox',
-  templateUrl: './chekbox.page.html',
+  
   styleUrls: ['./chekbox.page.scss'],
 })
 export class ChekboxPage implements OnInit {
